c6d9f1149a6d702c91fe9921e086bab4b5e874e2
Abdulaziz <suleymanovabdulaziz@gmail.com>
2022-02-18 20:23:33 +0000

    Now you can enter your personal information and your review which will be written under the heading “All reviews” with your name

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def reviews(request):
-    return render(request,'review.html')
+    name_1=request.GET.get("name")
+    email_1=request.GET.get("email")
+    review_1=request.GET.get("review")
+    print(f"Name:{name_1} , Email:{email_1} , Review:{review_1}")
+    return render(request,'review.html',{'name_1':name_1 , 'email_1':email_1, 'review_1':review_1})

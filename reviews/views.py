e331ef3d852469e01319ed9c9e864d29c62f5176
Abdulaziz <suleymanovabdulaziz@gmail.com>
2022-02-18 21:08:20 +0000

    Now while submitting your review,date and time is also asked and other users can see it while reading your review in the “All Reviews” section.And in that section,visual dividers were added.

--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,10 @@
+from sqlite3 import Date
 from django.shortcuts import render
 
 def reviews(request):
     name_1=request.GET.get("name")
     email_1=request.GET.get("email")
     review_1=request.GET.get("review")
-    print(f"Name:{name_1} , Email:{email_1} , Review:{review_1}")
-    return render(request,'review.html',{'name_1':name_1 , 'email_1':email_1, 'review_1':review_1})
+    date_time_1=request.GET.get("date")
+    print(f"Name:{name_1} , Date and Time:{date_time_1} , Email:{email_1} , Review:{review_1}")
+    return render(request,'review.html',{'name_1':name_1 , 'email_1':email_1, 'review_1':review_1, 'date_time_1':date_time_1})
